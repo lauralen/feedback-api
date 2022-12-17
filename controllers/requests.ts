5d95d7534b22c8d671eb41147704c3c3ce9b511d
lauralen <laurallenkutyte@gmail.com>
2022-12-17 19:03:24 +0200

    Create request

--- controllers/requests.ts
+++ controllers/requests.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import RequestModel from '../models/Request'
 
 const getRequests = (req: Request, res: Response) => {
   res.status(200).json({ success: true, message: 'Get all requests' })
@@ -10,8 +11,14 @@ const getRequest = (req: Request, res: Response) => {
     .json({ success: true, message: `Get request ${req.params.id}` })
 }
 
-const createRequest = (req: Request, res: Response) => {
-  res.status(200).json({ success: true, message: 'Create new request' })
+const createRequest = async (req: Request, res: Response) => {
+  try {
+    const request = await RequestModel.create(req.body)
+    res.status(201).json({ success: true, data: request })
+  } catch (error) {
+    res.status(400).json({success: false})
+  }
+  
 }
 
 const updateRequest = (req: Request, res: Response) => {
