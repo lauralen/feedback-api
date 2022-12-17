921137da743526e2b35c0549c6789194e8f4f835
lauralen <laurallenkutyte@gmail.com>
2022-12-17 19:05:10 +0200

    Get all requests

--- controllers/requests.ts
+++ controllers/requests.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express'
 import RequestModel from '../models/Request'
 
-const getRequests = (req: Request, res: Response) => {
-  res.status(200).json({ success: true, message: 'Get all requests' })
+const getRequests = async (req: Request, res: Response) => {
+  try {
+    const requests = await RequestModel.find()
+    
+    res.status(200).json({ success: true, data: requests })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 
 const getRequest = (req: Request, res: Response) => {
@@ -16,7 +22,7 @@ const createRequest = async (req: Request, res: Response) => {
     const request = await RequestModel.create(req.body)
     res.status(201).json({ success: true, data: request })
   } catch (error) {
-    res.status(400).json({success: false})
+    res.status(400).json({ success: false })
   }
   
 }
