f49934490cc21489522f051219557c561890b611
lauralen <laurallenkutyte@gmail.com>
2022-12-17 19:10:14 +0200

    Get a request by ID

--- controllers/requests.ts
+++ controllers/requests.ts
@@ -11,10 +11,20 @@ const getRequests = async (req: Request, res: Response) => {
   }
 }
 
-const getRequest = (req: Request, res: Response) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Get request ${req.params.id}` })
+const getRequest = async (req: Request, res: Response) => {
+  try {
+    const request = await RequestModel.findById(req.params.id)
+
+    // when id has valid format but doesn't exist existent in db
+    // fix returning { success: true, data: null }
+    if (!request) {
+      return res.status(400).json({ success: false})
+    }
+    
+    res.status(200).json({ success: true, data: request })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 
 const createRequest = async (req: Request, res: Response) => {
