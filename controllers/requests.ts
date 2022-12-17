a5378550cf39f7705b84bcd496f202b0e349d78c
lauralen <laurallenkutyte@gmail.com>
2022-12-17 19:14:51 +0200

    Update a request

--- controllers/requests.ts
+++ controllers/requests.ts
@@ -34,13 +34,23 @@ const createRequest = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(400).json({ success: false })
   }
-  
 }
 
-const updateRequest = (req: Request, res: Response) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Update request ${req.params.id}` })
+const updateRequest = async (req: Request, res: Response) => {
+  try {
+    const request = await RequestModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // get updated data in response
+      runValidators: true
+    })
+
+    if (!request) {
+      return res.status(400).json({ success: false})
+    }
+
+    res.status(200).json({ success: true, data: request })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 
 const deleteRequest = (req: Request, res: Response) => {
